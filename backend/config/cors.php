ecb83b7f4d8ef94bb239b24e0caa41485b54750b
Quitorio <marcalvinq@gmail.com>
2026-09-11 00:04:42 +0800

    Update

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,10 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        env('FRONTEND_URL', 'http://localhost:5173'),
-        'https://itb7-cabs-system.vercel.app', 
-    ],
+    'allowed_origins' => ['https://itb7-cabs-system.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
