61e89ea3abefd23eafc953cac5488da72e3f93ca
marcalvinq8-cpu <marcalvinq8@gmail.com>
2026-09-11 10:20:28 +0800

    Add protected facility seed debug route

---
+++ backend/app/Http/Controllers/DebugSeedController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+
+class DebugSeedController extends Controller
+{
+    public function seedFacilities(Request $request)
+    {
+        $expectedToken = (string) env('DEBUG_SEED_TOKEN');
+        $providedToken = (string) $request->query('token');
+
+        abort_unless(
+            $expectedToken !== '' && hash_equals($expectedToken, $providedToken),
+            404
+        );
+
+        Artisan::call('db:seed', [
+            '--class' => 'Database\\Seeders\\FacilitySeeder',
+            '--force' => true,
+        ]);
+
+        return response()->json(['output' => Artisan::output()]);
+    }
+}
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\DebugSeedController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/debug-seed-facilities', [DebugSeedController::class, 'seedFacilities']);
